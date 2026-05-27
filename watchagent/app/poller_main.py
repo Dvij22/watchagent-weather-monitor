1c690d5d66e584aceb45efabb1f786e135075625
dvij <dvij@birg.UNBC.ca>
2026-05-27 16:22:11 -0700

    added the docker containers

--- watchagent/app/poller_main.py
+++ watchagent/app/poller_main.py
@@ -24,7 +24,6 @@ def _configure_structlog() -> None:
         processors=[
             structlog.contextvars.merge_contextvars,
             structlog.stdlib.add_log_level,
-            structlog.stdlib.add_logger_name,
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.StackInfoRenderer(),
             structlog.processors.format_exc_info,
@@ -32,7 +31,7 @@ def _configure_structlog() -> None:
         ],
         wrapper_class=structlog.make_filtering_bound_logger(log_level),
         context_class=dict,
-        logger_factory=structlog.PrintLoggerFactory(sys.stdout),
+        logger_factory=structlog.WriteLoggerFactory(file=sys.stdout),
         cache_logger_on_first_use=True,
     )
 
